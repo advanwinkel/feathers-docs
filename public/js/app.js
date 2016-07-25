8913cbde03992779cd2d82a450d832fc7aa3cfb0
Ad <advanwinkel@gmail.com>
2016-07-25 16:35:06 +0200

    Added directory  uploads to .gitignore

--- public/js/app.js
+++ public/js/app.js
@@ -178,8 +178,8 @@ vm = new Vue({
   }
 })
 
-// Helper function for converting dataurl to blob
-//(see: http://stackoverflow.com/questions/6850276/how-to-convert-dataurl-to-file-object-in-javascript)
+// Helper function for converting dataurl to blob, so it can be saved using FileSaver.js
+//(from: http://stackoverflow.com/questions/6850276/how-to-convert-dataurl-to-file-object-in-javascript)
 function dataURLtoBlob(dataurl) {
     var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
         bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
